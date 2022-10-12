751cc2d8f0d1c4686ebd6b0e1da7e7bbe0750290
pazcalll <yazeedarifin@gmail.com>
2022-10-12 19:01:16 +0700

    initialization

---
+++ resources/views/index.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="copyright" content="MACode ID, https://macodeid.com/">
+
+    <title>Online Order</title>
+
+    <link rel="stylesheet" href="{{ asset('css/maicons.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('vendor/animate/animate.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/theme.css') }}">
+
+</head>
+<body>
+
+    <!-- Back to top button -->
+    <div class="back-to-top"></div>
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
+            <div class="container">
+                <a href="javascript:void(0)" class="navbar-brand">Penghitung <span class="text-primary">Pesanan Online</span></a>
+
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
+    </header>
+
+    <div class="page-section">
+        <div class="container">
+            <div class="row" style="display: flex; justify-content: center;">
+                <div class="col-lg-4">
+                    <div class="card-service wow fadeInUp">
+                        <div class="header">
+                            Pelanggan 1
+                        </div>
+                        <hr>
+                        <div class="body">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- .container -->
+    </div> <!-- .page-section -->
+
+    <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
+
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
+    <script src="{{ asset('js/google-maps.js') }}"></script>
+
+    <script src="{{ asset('vendor/wow/wow.min.js') }}"></script>
+
+    <script src="{{ asset('js/theme.js') }}"></script>
+  
+</body>
+</html>
