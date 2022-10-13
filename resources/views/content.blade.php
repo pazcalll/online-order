3c794e1ef9d50269bedf5120fc8ae9348873ee2c
pazcalll <yazeedarifin@gmail.com>
2022-10-13 11:36:21 +0700

    z-index debug

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -5,18 +5,18 @@
 
 <div class="container">
     <div class="row card-container mb-3" style="display: flex; justify-content: center;">
-        <div class="col-lg-6" style="border-radius: 30px" id="card1">
+        <div class="col-lg-6" style="border-radius: 30px">
             <button style="position: absolute; padding-top: 2px; padding-bottom: 2px; margin-top: 16px" class="btn btn-danger" onclick="deleteCard(this)">x</button>
             <div class="card-service border shadow" style="max-width: initial">
                 <div class="header" style="margin: 0">
-                    <input class="form-control" style="border-radius: 30px; justify-content: center; text-align: center" placeholder="Nama Pemesan" type="text" name="name" id="name">
+                    <input class="form-control" style="border-radius: 30px; justify-content: center; text-align: center" placeholder="Nama Pemesan" type="text">
                 </div>
                 <hr>
                 <div class="body" id="input-body1">
                     <div class="input-group mb-1" id="input-group1" data-menu-counter="1">
-                        <input class="form-control" size="40" style="width: 35%; font-size: 15px; height: 30px; border-top-left-radius: 30px; border-bottom-left-radius: 30px; justify-content: center; text-align: center" placeholder="Nama Menu" type="text" name="menu1" id="menu1">
-                        <input class="form-control" size="40" style="width: 25%; font-size: 15px; height: 30px; justify-content: center; text-align: center" placeholder="Harga" type="text" name="price1" id="price1">
-                        <input class="form-control" size="20" style="font-size: 15px; height: 30px; border-top-right-radius: 30px; border-bottom-right-radius: 30px; justify-content: center; text-align: center" placeholder="Jml" type="number" name="amount1" id="amount1">
+                        <input class="form-control" size="40" style="width: 35%; font-size: 15px; height: 30px; border-top-left-radius: 30px; border-bottom-left-radius: 30px; justify-content: center; text-align: center" placeholder="Nama Menu" type="text">
+                        <input class="form-control" size="40" style="width: 20%; font-size: 15px; height: 30px; justify-content: center; text-align: center" placeholder="Harga" type="number">
+                        <input class="form-control" size="20" style="font-size: 15px; height: 30px; border-top-right-radius: 30px; border-bottom-right-radius: 30px; justify-content: center; text-align: center" placeholder="Jml" type="number">
                         
                         <button onclick="deleteMenu(this)" type="button" class="ml-3 close" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
@@ -31,28 +31,48 @@
         </div>
     </div>
     <div class="row" style="position: sticky; ">
-        <button class="btn btn-info" style="width: 100%">
+        <button class="btn btn-info" style="width: 100%" data-toggle="modal" data-target="#calculateModal">
             <span class="mai-calculator"></span>
             Kalkulasi
         </button>
     </div>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="calculateModal" tabindex="-1" aria-labelledby="calculateModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="calculateModalLabel">Hasil Penghitungan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
     function cardAdder() {
         $('#card_adder').replaceWith(`
-            <div class="col-lg-6" style="border-radius: 30px" id="card${card_counter}">
+            <div class="col-lg-6 wow fadeIn" style="z-index:5; border-radius: 30px" id="card${card_counter}">
                 <button style="position: absolute; padding-top: 2px; padding-bottom: 2px; margin-top: 16px" class="btn btn-danger" onclick="deleteCard(this)">x</button>
                 <div class="card-service border shadow" style="max-width: initial">
                     <div class="header" style="margin: 0">
-                        <input class="form-control" style="border-radius: 30px; justify-content: center; text-align: center" placeholder="Nama Pemesan" type="text" name="name${card_counter}" id="name${card_counter}">
+                        <input class="form-control" style="border-radius: 30px; justify-content: center; text-align: center" placeholder="Nama Pemesan" type="text">
                     </div>
                     <hr>
                     <div class="body" id="input-body${card_counter}">
                         <div class="input-group mb-1" id="input-group${card_counter}">
-                            <input class="form-control" size="40" style="width: 35%; font-size: 15px; height: 30px; border-top-left-radius: 30px; border-bottom-left-radius: 30px; justify-content: center; text-align: center" placeholder="Nama Menu" type="text" name="menu${card_counter}" id="menu${card_counter}">
-                            <input class="form-control" size="40" style="width: 25%; font-size: 15px; height: 30px; justify-content: center; text-align: center" placeholder="Harga" type="text" name="price${card_counter}" id="price${card_counter}">
-                            <input class="form-control" size="20" style="font-size: 15px; height: 30px; border-top-right-radius: 30px; border-bottom-right-radius: 30px; justify-content: center; text-align: center" placeholder="Jml" type="number" name="amount${card_counter}" id="amount${card_counter}">
+                            <input class="form-control" size="40" style="width: 35%; font-size: 15px; height: 30px; border-top-left-radius: 30px; border-bottom-left-radius: 30px; justify-content: center; text-align: center" placeholder="Nama Menu" type="text">
+                            <input class="form-control" size="40" style="width: 20%; font-size: 15px; height: 30px; justify-content: center; text-align: center" placeholder="Harga" type="number">
+                            <input class="form-control" size="20" style="font-size: 15px; height: 30px; border-top-right-radius: 30px; border-bottom-right-radius: 30px; justify-content: center; text-align: center" placeholder="Jml" type="number">
                             
                             <button onclick="deleteMenu(this)" type="button" class="ml-3 close" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
@@ -69,12 +89,11 @@
         card_counter += 1
     }
     function menuAdder(e) {
-        console.log(e)
         $(e).replaceWith(`
             <div class="input-group mb-1" id="input-group${this.card_counter}">
-                <input class="form-control" size="40" style="width: 35%; font-size: 15px; height: 30px; border-top-left-radius: 30px; border-bottom-left-radius: 30px; justify-content: center; text-align: center" placeholder="Nama Menu" type="text" name="menu${this.card_counter}" id="menu${this.card_counter}">
-                <input class="form-control" size="40" style="width: 25%; font-size: 15px; height: 30px; justify-content: center; text-align: center" placeholder="Harga" type="text" name="price${this.card_counter}" id="price${this.card_counter}">
-                <input class="form-control" size="20" style="font-size: 15px; height: 30px; border-top-right-radius: 30px; border-bottom-right-radius: 30px; justify-content: center; text-align: center" placeholder="Jml" type="number" name="amount${this.card_counter}" id="amount${this.card_counter}">
+                <input class="form-control" size="40" style="width: 35%; font-size: 15px; height: 30px; border-top-left-radius: 30px; border-bottom-left-radius: 30px; justify-content: center; text-align: center" placeholder="Nama Menu" type="text">
+                <input class="form-control" size="40" style="width: 20%; font-size: 15px; height: 30px; justify-content: center; text-align: center" placeholder="Harga" type="number">
+                <input class="form-control" size="20" style="font-size: 15px; height: 30px; border-top-right-radius: 30px; border-bottom-right-radius: 30px; justify-content: center; text-align: center" placeholder="Jml" type="number">
                 
                 <button onclick="deleteMenu(this)" type="button" class="ml-3 close" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,13 +2,23 @@
 <div class="back-to-top"></div>
 
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white wow fadeInDown">
         <div class="container">
             <a href="javascript:void(0)" class="navbar-brand">Penghitung <span class="text-primary">Pesanan Online</span></a>
 
             <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <div class="navbar-collapse collapse" id="navbarContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="javascript:void(0)">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0)">Riwayat</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </header>
